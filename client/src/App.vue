dcd3b52cc2d51437cf31edfdd87c7a80b8fc71cb
Lydia Lind <lydia.lind@hey.com>
2020-11-18 17:02:32 +0100

    Add some styling to main App

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <Test message="Fullstack yay!"/>
+  <main>
+    <Test message="Fullstack yay!"/>
+  </main>
 </template>
 
 <script>
@@ -15,4 +17,21 @@ export default {
 </script>
 
 <style>
+*{
+  box-sizing: border-box;
+  margin: 0px;
+  padding: 0px;
+}
+body, html, #app{
+  height: 100vh;
+  width: 100vw;
+}
+main{
+  box-shadow: 0px 0px 21px -1px rgba(0,0,0,0.69);
+  min-height: 100%;
+  margin: 0 auto;
+  width: 50%;
+  -webkit-box-shadow: 0px 0px 21px -1px rgba(0,0,0,0.69); 
+
+}
 </style>

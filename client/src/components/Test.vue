802faf12049b3eee6bf61a707020a519ac7967cc
Lydia Lind <lydia.lind@hey.com>
2020-11-18 09:43:14 +0100

    cleaning up, removing unnecessary init code

---
+++ client/src/components/Test.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<script>
+export default {
+    name: 'Test',
+    props: {
+        message: String
+    }
+}
+</script>
+
+<style scoped>
+h1{
+    color: red;
+}
+</style>

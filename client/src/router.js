d2704ae3b6ba019cbc70f84a410af8872ce9f866
Lydia Lind <lydia.lind@hey.com>
2020-12-04 14:36:57 +0100

    Add navigation guards

--- client/src/router.js
+++ client/src/router.js
@@ -10,19 +10,37 @@ import EditRecipe from './pages/EditRecipe.vue'
 import RecipeDetail from './pages/RecipeDetail.vue'
 import SignUp from './pages/SignUp.vue'
 
+import store from './store.js'
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: SignIn },
     { path: '/sign-up', component: SignUp },
-    { path: '/home', component: Home },
-    { path: '/my-recipes', component: MyRecipes },
-    { path: '/my-recipes/add-recipe', component: AddRecipe },
-    { path: '/my-recipes/edit-recipe/:id', component: EditRecipe },
-    { path: '/my-recipes/:id', component: RecipeDetail },
-    { path: '/my-profile', component: MyProfile },
+    { path: '/home', component: Home, meta: { requiresAuth: true } },
+    { path: '/my-recipes', component: MyRecipes, meta: { requiresAuth: true } },
+    {
+      path: '/my-recipes/add-recipe',
+      component: AddRecipe,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/my-recipes/edit-recipe/:id',
+      component: EditRecipe,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/my-recipes/:id',
+      component: RecipeDetail,
+      meta: { requiresAuth: true }
+    },
+    { path: '/my-profile', component: MyProfile, meta: { requiresAuth: true } },
     { path: '/:notFound(.*)', component: NotFound }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  to.meta.requiresAuth && !store.getters.isAuth ? next('/') : next()
 })
 
 export default router

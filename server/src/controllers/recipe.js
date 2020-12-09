03e43113b2d8a75dce50e2c22b89854d95054cfd
Lydia Lind <lydia.lind@hey.com>
2020-12-09 10:28:47 +0100

    Refactoring and cleaning up recipe controllers

--- server/src/controllers/recipe.js
+++ server/src/controllers/recipe.js
@@ -1,6 +1,9 @@
 import Recipe from '../models/Recipe.js'
 import User from '../models/User.js'
 import Middlewares from '../middleware/Middlewares.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const addRecipe = async (req, res, _next) => {
   try {
@@ -31,45 +34,51 @@ const addRecipeImage = async (req, res, _next) => {
     } else {
       res
         .status(200)
-        .send({ src: `http://localhost:3001/${req.file.filename}` })
+        .send({ src: `${process.env.BASE_URL}${req.file.filename}` })
     }
   })
 }
 
-const getRecipes = async (req, res, next) => {
+const getRecipes = async (req, res, _next) => {
   try {
-    const user = await User.findOne({ firebaseId: req.query.firebaseId }).exec()
+    const user = await User.findOne({ firebaseId: req.query.firebaseId })
     const recipes = await Recipe.find(
       { userId: user._id },
       'details ingredients title description imageURL'
-    ).exec()
+    )
     res.status(200).send(recipes)
   } catch (error) {
-    next(error)
+    res.status(404).send({ message: 'Could not get recipes' })
   }
 }
 
-const getRecipe = async (req, res, next) => {
+const getRecipe = async (req, res, _next) => {
   try {
-    const recipe = await Recipe.findById(req.query.recipeId).exec()
-    recipe ? res.status(200).send(recipe) : res.sendStatus(404)
+    const recipe = await Recipe.findById(req.query.recipeId)
+    if (recipe) {
+      res.status(200).send(recipe)
+    } else {
+      throw new Error('Could not find recipe')
+    }
   } catch (error) {
-    //Refactor to custom error send instead of calling custom error handler
-    next(error)
+    res.status(404).send({ message: error.message })
   }
 }
 
 const deleteRecipe = async (req, res, _next) => {
   try {
-    const result = await Recipe.findByIdAndDelete(req.params.recipeId)
-    console.log(result)
-    res.sendStatus(204)
+    const deletedRecipe = await Recipe.findByIdAndDelete(req.params.recipeId)
+    if (deletedRecipe) {
+      res.sendStatus(204)
+    } else {
+      throw new Error('Could not find a recipe to delete')
+    }
   } catch (error) {
-    res.status(400).send({ message: 'Could not delete recipe' })
+    res.status(400).send({ message: error.message })
   }
 }
 const updateRecipe = async (req, res, _next) => {
-  const updatedRecipe = {
+  const newInformation = {
     title: req.body.title,
     imageURL: req.body.imageURL,
     ingredients: req.body.ingredients,
@@ -81,12 +90,20 @@ const updateRecipe = async (req, res, _next) => {
     }
   }
   try {
-    const result = await Recipe.findByIdAndUpdate(req.body._id, updatedRecipe, {
-      new: true
-    })
-    res.status(200).send(result)
+    const updatedRecipe = await Recipe.findByIdAndUpdate(
+      req.body._id,
+      newInformation,
+      {
+        new: true
+      }
+    )
+    if (updatedRecipe) {
+      res.status(200).send(updatedRecipe)
+    } else {
+      throw new Error('Could not update recipe')
+    }
   } catch (error) {
-    res.status(400).send({ message: 'Could not update recipe' })
+    res.status(400).send({ message: error.message })
   }
 }
 

1db24bc2bc7be75e96cf015fb7c24238f78d44a1
Lydia Lind <lydia.lind@hey.com>
2020-12-09 09:39:20 +0100

    Add timestamps for recipes in mongo

--- server/src/models/Recipe.js
+++ server/src/models/Recipe.js
@@ -2,17 +2,22 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
-const recipeSchema = new Schema({
-  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  title: { type: String, required: true },
-  imageURL: { type: String },
-  ingredients: [String],
-  description: String,
-  details: {
-    categories: [String],
-    qualities: [String],
-    timeToCook: String
+const recipeSchema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    title: { type: String, required: true },
+    imageURL: { type: String },
+    ingredients: [String],
+    description: String,
+    details: {
+      categories: [String],
+      qualities: [String],
+      timeToCook: String
+    }
+  },
+  {
+    timestamps: true
   }
-})
+)
 
 export default mongoose.model('Recipe', recipeSchema)

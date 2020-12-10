6048cb31300fd020d8fb95760aad6f8181afa0f3
Lydia Lind <lydia.lind@hey.com>
2020-12-10 16:47:02 +0100

    remove unneccessary information from user model

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -6,8 +6,6 @@ const userSchema = new Schema({
   firebaseId: { type: String, required: true },
   firstName: { type: String },
   lastName: { type: String },
-  email: { type: String },
-  phoneNumber: { type: String },
   foodPreferences: [String]
 })
 

aaafd2e311da46240eb68bbca267a25aef5c70c5
Lydia Lind <lydia.lind@hey.com>
2020-12-10 10:28:21 +0100

    add form wrapper

--- client/src/components/ChangeEmail.vue
+++ client/src/components/ChangeEmail.vue
@@ -1,29 +1,30 @@
 <template>
-  <form class="ui form" @submit="changeEmail"></form>
-  <div class="field">
-    <p>Current email: {{ currentEmail }}</p>
-  </div>
-  <div class="field">
-    <label>New Email</label>
-    <input
-      type="email"
-      name="new-email"
-      placeholder="New Email"
-      v-model="newEmail"
-    />
-  </div>
-  <div class="field">
-    <label>Confirm New Email</label>
-    <input
-      type="email"
-      name="new-email-confirmed"
-      placeholder="Confirmed Email"
-      v-model="confirmedEmail"
-    />
-  </div>
-  <div class="field">
-    <base-button>Change Email</base-button>
-  </div>
+  <form class="ui form" @submit="changeEmail">
+    <div class="field">
+      <p>Current email: {{ currentEmail }}</p>
+    </div>
+    <div class="field">
+      <label>New Email</label>
+      <input
+        type="email"
+        name="new-email"
+        placeholder="New Email"
+        v-model="newEmail"
+      />
+    </div>
+    <div class="field">
+      <label>Confirm New Email</label>
+      <input
+        type="email"
+        name="new-email-confirmed"
+        placeholder="Confirmed Email"
+        v-model="confirmedEmail"
+      />
+    </div>
+    <div class="field">
+      <base-button>Change Email</base-button>
+    </div>
+  </form>
 </template>
 
 <script>
--- client/src/components/ChangePassword.vue
+++ client/src/components/ChangePassword.vue
@@ -1,24 +1,25 @@
 <template>
-  <form class="ui form" @submit="changePassword"></form>
-  <div class="field">
-    <label>Old Password</label>
-    <input type="password" name="old-password" placeholder="Old Password" />
-  </div>
-  <div class="field">
-    <label>New Password</label>
-    <input type="password" name="new-password" placeholder="New Password" />
-  </div>
-  <div class="field">
-    <label>Confirm New Password</label>
-    <input
-      type="password"
-      name="new-password-confirmed"
-      placeholder="Confirmed Password"
-    />
-  </div>
-  <div class="field">
-    <base-button>Change Password</base-button>
-  </div>
+  <form class="ui form" @submit="changePassword">
+    <div class="field">
+      <label>Old Password</label>
+      <input type="password" name="old-password" placeholder="Old Password" />
+    </div>
+    <div class="field">
+      <label>New Password</label>
+      <input type="password" name="new-password" placeholder="New Password" />
+    </div>
+    <div class="field">
+      <label>Confirm New Password</label>
+      <input
+        type="password"
+        name="new-password-confirmed"
+        placeholder="Confirmed Password"
+      />
+    </div>
+    <div class="field">
+      <base-button>Change Password</base-button>
+    </div>
+  </form>
 </template>
 
 <script>

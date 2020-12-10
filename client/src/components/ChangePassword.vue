c5c55d3540c30174202a09535db9571a44885451
Lydia Lind <lydia.lind@hey.com>
2020-12-10 10:06:19 +0100

    Add changePassword component

---
+++ client/src/components/ChangePassword.vue
@@ -0,0 +1,41 @@
+<template>
+  <form class="ui form" @submit="changePassword"></form>
+  <div class="field">
+    <label>Old Password</label>
+    <input type="password" name="old-password" placeholder="Old Password" />
+  </div>
+  <div class="field">
+    <label>New Password</label>
+    <input type="password" name="new-password" placeholder="New Password" />
+  </div>
+  <div class="field">
+    <label>Confirm New Password</label>
+    <input
+      type="password"
+      name="new-password-confirmed"
+      placeholder="Confirmed Password"
+    />
+  </div>
+  <div class="field">
+    <base-button>Change Password</base-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      oldPassword: '',
+      newPassword: '',
+      confirmedPassword: ''
+    }
+  },
+  methods: {
+    changePassword() {
+      console.log('Changing password :D')
+    }
+  }
+}
+</script>
+
+<style></style>

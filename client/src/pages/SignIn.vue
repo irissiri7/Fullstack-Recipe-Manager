1ca5744447453c10cc09c548b90183a2d4ab15ec
Lydia Lind <lydia.lind@hey.com>
2020-12-18 14:59:53 +0100

    Optimize signIn page, now sign up is baked in there  and also reset password

--- client/src/pages/SignIn.vue
+++ client/src/pages/SignIn.vue
@@ -3,41 +3,84 @@
     <base-dialog-card v-if="feedback.message" :style="feedback.style">
       {{ feedback.message }}
     </base-dialog-card>
-    <h1>Sign In</h1>
-    <form class="ui form" @submit.prevent="signIn">
+    <h1 v-if="mode == 'sign in'">Sign In</h1>
+    <h1 v-if="mode == 'sign up'">Sign Up</h1>
+    <h1 v-if="mode == 'reset password'">Reset Password</h1>
+    <form class="ui form" @submit.prevent="submitForm">
       <div class="field">
         <label>Email</label>
-        <div class="field">
-          <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            v-model="email"
-            required
-          />
-        </div>
+        <input
+          v-model="email"
+          type="email"
+          name="email"
+          placeholder="name@email.com"
+          required
+        />
+      </div>
+      <div class="field" v-if="mode == 'sign up' || mode == 'sign in'">
         <label>Password</label>
-        <div class="field">
-          <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            v-model="password"
-            required
-          />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          v-model="password"
+          minlength="6"
+          required
+        />
+      </div>
+      <div class="field" v-if="mode == 'sign up'">
+        <label>Confirm password</label>
+        <input
+          v-model="confirmedPassword"
+          type="password"
+          name="confirmedPassword"
+          placeholder="Confirm Password"
+          minlength="6"
+          required
+        />
+      </div>
+      <div class="field" v-if="mode == 'sign in'">
+        <base-button
+          type="submit"
+          class="same-width"
+          :class="{ disabled: !validForm }"
+          >Sign In</base-button
+        >
+      </div>
+      <div class="field" v-if="mode == 'sign up'">
+        <div class="ui checkbox">
+          <input type="checkbox" name="example" v-model="acceptsTermsOfUse" />
+          <label>I agree to terms of use</label>
         </div>
       </div>
-      <div class="field">
-        <base-button class="same-width">Sign In</base-button>
+      <div class="field" v-if="mode == 'sign up'">
+        <base-button type="submit" :class="{ disabled: !validForm }">
+          Sign Up
+        </base-button>
       </div>
-      <div class="field">
+      <div class="field" v-if="mode == 'reset password'">
+        <base-button type="submit" :class="{ disabled: !validForm }">
+          Reset Password
+        </base-button>
+      </div>
+      <div class="field" v-if="mode == 'sign up' || mode == 'reset password'">
+        <base-button type="button" mode="link" @click="mode = 'sign in'"
+          >Back to log in</base-button
+        >
+      </div>
+      <div class="field" v-if="mode == 'sign in'">
         <div class="flex corner">
-          <base-button @click="resetPassword" mode="link"
+          <base-button
+            type="button"
+            mode="link"
+            @click="mode = 'reset password'"
             >Forgot password?</base-button
           >
-          <div class="flex">
+          <div class="flex" v-if="mode == 'sign in'">
             <p>Not a user?</p>
-            <base-button link to="/sign-up">Sign Up</base-button>
+            <base-button type="button" mode="link" @click="mode = 'sign up'"
+              >Sign Up</base-button
+            >
           </div>
         </div>
       </div>
@@ -54,16 +97,45 @@ dotenv.config()
 export default {
   data() {
     return {
-      email: 'dwight@example.com',
-      password: '111111',
+      mode: 'sign in',
+      email: '',
+      password: '',
+      confirmedPassword: null,
+      acceptsTermsOfUse: false,
       feedback: {
         message: null,
         style: null
       }
     }
   },
+  computed: {
+    validForm() {
+      if (this.mode == 'sign in') {
+        return !!this.email && this.password
+      } else if (this.mode == 'sign up') {
+        return (
+          this.email &&
+          this.password &&
+          this.confirmedPassword &&
+          this.acceptsTermsOfUse &&
+          this.password === this.confirmedPassword
+        )
+      } else if (this.mode == 'reset password') {
+        return !!this.email
+      } else {
+        return false
+      }
+    }
+  },
   methods: {
+    submitForm() {
+      if (this.mode == 'sign in') this.signIn()
+      else if (this.mode == 'sign up') this.signUp()
+      else if (this.mode == 'reset password') this.resetPassword()
+      else return
+    },
     signIn() {
+      console.log('sign in')
       this.$store
         .dispatch('signIn', {
           email: this.email,
@@ -72,21 +144,47 @@ export default {
         .then(() => {
           this.$router.push('/home')
         })
-        .catch(_error => {
+        .catch(error => {
+          console.log(error)
           this.feedback.message = 'Could not sign in. Check your credentials.'
           this.feedback.style = 'error'
+        })
+    },
+    signUp() {
+      console.log('sign up')
+      this.$store
+        .dispatch('signUp', {
+          email: this.email,
+          password: this.password
+        })
+        .then(() => {
+          this.feedback.style = 'informational'
+          this.feedback.message =
+            'Sign up successful! You are now welcome to sign in'
+          ;(this.email = ''),
+            (this.password = ''),
+            (this.confirmedPassword = ''),
+            (this.acceptsTermsOfUse = false),
+            (this.mode = 'sign in')
+        })
+        .catch(_error => {
+          this.feedback.style = 'error'
+          this.feedback.message =
+            'Something went wrong :( ... Could not sign up new user'
         })
     },
     async resetPassword() {
       try {
         await axios.post(
           `https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=${process.env.VUE_APP_FIREBASE_API_KEY}`,
-          { email: 'lydia.lind@hey.com', requestType: 'PASSWORD_RESET' }
+          { email: this.email, requestType: 'PASSWORD_RESET' }
         )
-        this.feedback.style = 'informatonal'
+        this.feedback.style = 'informational'
         this.feedback.message =
           'A password reset mail has been sent, check your inbox'
+        this.email = ''
       } catch (error) {
+        console.log(error)
         this.feedback.style = 'error'
         this.feedback.message =
           'Something went wrong, could not send password reset mail'
--- client/src/router.js
+++ client/src/router.js
@@ -8,7 +8,6 @@ import SignIn from './pages/SignIn.vue'
 import AddRecipe from './pages/AddRecipe.vue'
 import EditRecipe from './pages/EditRecipe.vue'
 import RecipeDetail from './pages/RecipeDetail.vue'
-import SignUp from './pages/SignUp.vue'
 
 import store from './store.js'
 
@@ -16,7 +15,6 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: SignIn },
-    { path: '/sign-up', component: SignUp },
     { path: '/home', component: Home, meta: { requiresAuth: true } },
     { path: '/my-recipes', component: MyRecipes, meta: { requiresAuth: true } },
     {

297af5a16c6cccf6b3cf63602792cfbf0a3a1e98
Lydia Lind <lydia.lind@hey.com>
2021-01-05 12:59:55 +0100

    Rename SignIn.vue to more appropriate Authentation.vue

--- client/src/router.js
+++ client/src/router.js
@@ -4,7 +4,7 @@ import Home from './pages/Home.vue'
 import MyRecipes from './pages/MyRecipes.vue'
 import MyProfile from './pages/MyProfile.vue'
 import NotFound from './pages/NotFound.vue'
-import SignIn from './pages/SignIn.vue'
+import Authentication from './pages/Authentication.vue'
 import AddRecipe from './pages/AddRecipe.vue'
 import EditRecipe from './pages/EditRecipe.vue'
 import RecipeDetail from './pages/RecipeDetail.vue'
@@ -14,7 +14,7 @@ import store from './store.js'
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: SignIn },
+    { path: '/', component: Authentication },
     { path: '/home', component: Home, meta: { requiresAuth: true } },
     { path: '/my-recipes', component: MyRecipes, meta: { requiresAuth: true } },
     {

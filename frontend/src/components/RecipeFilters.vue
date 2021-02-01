2efa1079f7a2afe509f0d25e4f78233d360e335a
Lydia Lind <lydia.lind@hey.com>
2021-02-01 14:17:16 +0100

    css fix of RecipeFilter so that checkboxes don't look badly aligned

--- frontend/src/components/RecipeFilters.vue
+++ frontend/src/components/RecipeFilters.vue
@@ -1,49 +1,56 @@
 <template>
   <base-card>
-    <h2>Select filters</h2>
-    <h4>Category</h4>
-    <div
-      class="ui checkbox"
-      v-for="category in recipeDetailOptions.categories"
-      :key="category"
-    >
-      <input
-        type="checkbox"
-        :value="category.toLowerCase()"
-        v-model="selectedCategories"
-        @change="filtering"
-      />
-      <label>{{ category }}</label>
+    <h2 class="title">Select filters</h2>
+    <div class="grid">
+      <div class="filter-group">
+        <h4>Category</h4>
+        <div
+          class="ui checkbox"
+          v-for="category in recipeDetailOptions.categories"
+          :key="category"
+        >
+          <input
+            type="checkbox"
+            :value="category.toLowerCase()"
+            v-model="selectedCategories"
+            @change="filtering"
+          />
+          <label>{{ category }}</label>
+        </div>
+      </div>
+      <div class="filter-group">
+        <h4>Quality</h4>
+        <div
+          class="ui checkbox"
+          v-for="quality in recipeDetailOptions.qualities"
+          :key="quality"
+        >
+          <input
+            type="checkbox"
+            :value="quality.toLowerCase()"
+            v-model="selectedQualities"
+            @change="filtering"
+          />
+          <label>{{ quality }}</label>
+        </div>
+      </div>
+      <div class="filter-group">
+        <h4>Time to cook</h4>
+        <div
+          class="ui radio checkbox"
+          v-for="option in recipeDetailOptions.timeToCook"
+          :key="option"
+        >
+          <input
+            type="radio"
+            :value="option"
+            v-model="selectedTimeToCook"
+            @change="filtering"
+          />
+          <label>{{ option }}</label>
+        </div>
+      </div>
     </div>
-    <h4>Quality</h4>
-    <div
-      class="ui checkbox"
-      v-for="quality in recipeDetailOptions.qualities"
-      :key="quality"
-    >
-      <input
-        type="checkbox"
-        :value="quality.toLowerCase()"
-        v-model="selectedQualities"
-        @change="filtering"
-      />
-      <label>{{ quality }}</label>
-    </div>
-    <h4>Time to cook</h4>
-    <div
-      class="ui radio checkbox"
-      v-for="option in recipeDetailOptions.timeToCook"
-      :key="option"
-    >
-      <input
-        type="radio"
-        :value="option"
-        v-model="selectedTimeToCook"
-        @change="filtering"
-      />
-      <label>{{ option }}</label>
-    </div>
-
     <div class="flex clear-btn-cnt">
       <base-button class="compact mini alert" @click="clearFilters"
         >Clear filters</base-button
@@ -106,10 +113,28 @@ export default {
 </script>
 
 <style scoped>
+.title {
+  border-bottom: 1px solid lightgray;
+}
 .ui.checkbox {
   margin-left: 5px;
+  margin-bottom: 5px;
+  display: block;
 }
 .clear-btn-cnt {
   justify-content: flex-end;
+  margin-top: 1rem;
+}
+
+.grid {
+  display: grid;
+  grid-gap: 15px;
+  grid-template-columns: repeat(3, 1fr);
+}
+
+@media screen and (max-width: 1100px) {
+  .grid {
+    grid-template-columns: repeat(1, 1fr);
+  }
 }
 </style>

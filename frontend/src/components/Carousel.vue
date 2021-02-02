a825f0a02077a509a188ded29cde5ebbcc1eb535
Lydia Lind <lydia.lind@hey.com>
2021-02-02 14:01:26 +0100

    Implement carousel instead of crappy Spoonacular images

---
+++ frontend/src/components/Carousel.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="carousel-view">
+    <transition-group class="carousel" tag="div">
+      <div v-for="num in images" class="slide" :key="num">
+        <base-image
+          :src="require(`@/assets/img${num}.jpg`)"
+          alt="dish"
+          :height="30"
+        ></base-image>
+      </div>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      images: [1, 2, 3, 4, 5]
+    }
+  },
+  created() {
+    setInterval(this.next, 3000)
+  },
+  methods: {
+    next() {
+      const first = this.images.shift()
+      this.images = this.images.concat(first)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.carousel-view {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.carousel {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+}
+.slide {
+  /* flex: 0 0 20em; */
+  min-width: 100%;
+  height: 100%;
+  margin: 1em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: transform 0.7s ease-in-out;
+}
+.slide:first-of-type {
+  opacity: 0;
+}
+.slide:last-of-type {
+  opacity: 0;
+}
+</style>
--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -16,51 +16,29 @@
           <em>always</em> available.</strong
         >
       </p>
-      <base-image :src="src" alt="dish" :height="25" />
-      <p class="side-note">
-        This image was provided by
-        <a href="https://spoonacular.com/food-api/" target="_blank"
-          >Spoonacular API</a
-        >
-        Check out the recipe
-        <a :href="spoonacularSource" target="_blank">here</a>
-      </p>
+      <carousel></carousel>
     </base-card>
   </div>
 </template>
 
 <script>
 import client from '../util/Client.js'
-import axios from 'axios'
 import dotenv from 'dotenv'
 
 import CursiveHeader from '../components/CursiveHeader.vue'
+import Carousel from '../components/Carousel.vue'
 
 dotenv.config()
 
 export default {
-  components: { CursiveHeader },
+  components: { CursiveHeader, Carousel },
   data() {
     return {
-      firstName: null,
-      recipe: null
-    }
-  },
-  computed: {
-    src() {
-      return this.recipe
-        ? this.recipe.image
-        : 'https://peacemakersnetwork.org/wp-content/uploads/2019/09/placeholder.jpg'
-    },
-    spoonacularSource() {
-      return this.recipe
-        ? this.recipe.spoonacularSourceUrl
-        : 'https://spoonacular.com/'
+      firstName: null
     }
   },
   created() {
     this.fetchData()
-    this.fetchImage()
   },
   methods: {
     async fetchData() {
@@ -70,28 +48,9 @@ export default {
       } catch (error) {
         console.log(error)
       }
-    },
-    //Refactor to new client class, new client class or let be??
-    async fetchImage() {
-      try {
-        const response = await axios.get(
-          `https://api.spoonacular.com/recipes/random?number=1&apiKey=${process.env.VUE_APP_SPOONACULAR_API_KEY}`
-        )
-        this.recipe = response.data.recipes[0]
-      } catch (error) {
-        console.log(error)
-      }
     }
   }
 }
 </script>
 
-<style scoped>
-p {
-  font-size: 14px;
-}
-
-.side-note {
-  font-size: 12px;
-}
-</style>
+<style></style>
